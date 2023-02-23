f7fe3476b83b3b7ea1cb905c8aa1dfbdcd34875f
SeaBlooms <colinblumer@gmail.com>
2023-02-23 12:30:15 -0700

    Update J1_listIntegrationsRawData.py
    added try excepts

--- J1_listIntegrationsRawData.py
+++ J1_listIntegrationsRawData.py
@@ -10,10 +10,22 @@ headers = {
         "Jupiterone-Account": acct
         }
 
-r = requests.get("https://api.us.jupiterone.io/entities/4655ea65-f7be-4393-9ff6-98a14b304183/raw-data",
-                 headers=headers, verify=True)
-print(r.content)
+try:
+        r = requests.get("https://api.us.jupiterone.io/entities/4655ea65-f7be-4393-9ff6-98a14b304183/raw-data",
+                        headers=headers, verify=True)
+        
+        print(r.content)
 
-r = requests.get("https://api.us.jupiterone.io/entities/4655ea65-f7be-4393-9ff6-98a14b304183/raw-data-versions",
-                 headers=headers, verify=True)
-print(r.content)
+except Exception as e:
+        
+        print("GET request failed. Exception: {}".format(e))
+
+try:
+        r = requests.get("https://api.us.jupiterone.io/entities/4655ea65-f7be-4393-9ff6-98a14b304183/raw-data-versions",
+                        headers=headers, verify=True)
+
+        print(r.content)
+
+except Exception as e:
+        
+        print("GET request failed. Exception: {}".format(e))
